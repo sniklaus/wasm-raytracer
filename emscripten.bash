82363e0e08fb79f5c03096c1b31bfeb6cce87cef
Simon Niklaus <simon.niklaus@outlook.com>
2021-10-12 21:20:26 -0700

    no message

--- emscripten.bash
+++ emscripten.bash
@@ -4,5 +4,5 @@
 
 source ~/emscripten/emsdk_env.sh
 
-emcc -O2 -s WASM=0 emscripten.cpp -o emscripten-asm.html
-emcc -O2 -s WASM=1 emscripten.cpp -o emscripten-wasm.html
+emcc -O2 --memory-init-file 0 -s WASM=0 --bind -s MODULARIZE=1 -s EXPORT_NAME="'Assembly'" -s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap', '_render', '_uniform1i', '_uniform1f', '_uniform3fv']" emscripten.cpp -o emscripten-asm.js
+emcc -O2 --memory-init-file 0 -s WASM=1 --bind -s MODULARIZE=1 -s EXPORT_NAME="'Webassembly'" -s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap', '_render', '_uniform1i', '_uniform1f', '_uniform3fv']" emscripten.cpp -o emscripten-wasm.js
